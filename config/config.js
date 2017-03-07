3dad61bdc364f0995ec80f06863907dc7f2a0d61
hamzatei <ht@tradelab.fr>
2017-03-07 17:05:07 +0100

    Add config.js for security.

---
+++ config/config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* global module */
+
+var config = {
+  host            : '', // Google cloud SQL Server
+  user            : '',
+  password        : '',
+  database        : ''
+};
+
+module.exports = config;
